236367f0ae636250f035934f0aade769de46efeb
Pauline Vos <pvos88@gmail.com>
2022-03-23 17:27:43 +0100

    Create a payload for a payment request
    And print the created payload ID

--- start.php
+++ start.php
@@ -1,10 +1,28 @@
 <?php
 
+use Xrpl\XummSdkPhp\Payload\CustomMeta;
+use Xrpl\XummSdkPhp\Payload\Options;
+use Xrpl\XummSdkPhp\Payload\Payload;
+use Xrpl\XummSdkPhp\XummSdk;
+
 require_once './vendor/autoload.php';
 
-echo "Hello World\n";
+$sdk = new XummSdk();
 
-$sdk = new \Xrpl\XummSdkPhp\XummSdk();
-$pong = $sdk->ping();
+$payment = new Payload(
+    transactionBody: [
+        'TransactionType' => 'Payment',
+        'Destination' => 'rGBiHBoEs238W7H1b4gMey55He97kWcoUb' // Use your own address here
+    ],
+    options: new Options(
+        immutable: true,
+    ),
+    customMeta: new CustomMeta(
+        uniqid(),
+        'Hi! Can you pay me please? Thanks! ❤️',
+    )
+);
 
-echo "Application name: " . $pong->application->name;
+$createdPayment = $sdk->createPayload($payment);
+$url = $createdPayment->next->always;
+echo "Sign request: ${url}\n";
